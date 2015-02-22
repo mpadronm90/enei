d30a5c0da43fb89c209b57882711d4635d14ebc6
Pablo Guijarro Ramirez <sky10pg@gmail.com>
2015-02-22 11:10:07 +0100

    Añadido progressbar

--- mobile/src/main/java/sabadell/grupo18/enei/MainActivity.java
+++ mobile/src/main/java/sabadell/grupo18/enei/MainActivity.java
@@ -1,6 +1,8 @@
 package sabadell.grupo18.enei;
 
 import android.app.FragmentManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,15 +21,22 @@ public class MainActivity extends ActionBarActivity {
     private RecyclerView lstOferta;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager manager;
+    private ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      
 
         Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        bar= (ProgressBar) findViewById(R.id.progressBar);
+        bar.setMax(100);
+        changeProgressBar(bar,25);
+
+
+
         lstOferta= (RecyclerView) findViewById(R.id.lstOferta);
 
         manager=new LinearLayoutManager(this);
@@ -42,6 +53,20 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void changeProgressBar(ProgressBar bar, int value){
+        bar.setProgress(25);
+        if(bar.getProgress()/bar.getMax()<40){
+            bar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+
+
+        }else if(bar.getProgress()/bar.getMax()>40 && bar.getProgress()/bar.getMax()<60){
+            bar.getProgressDrawable().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
+        }else{
+            bar.getProgressDrawable().setColorFilter(Color.GREEN, PorterDuff.Mode.SRC_IN);
+        }
+
     }
 
     @Override
